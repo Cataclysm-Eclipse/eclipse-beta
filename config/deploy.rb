5b08b85b964b24b7e97cd8dc418cf9a419d2634a
Cataclysm Deploy <deploy@Cataclysm.io>
2015-12-11 23:19:18 -0500

    fix production issues hopefully

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :deploy_to, '/home/deploy/eclipse-beta'
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+set :rbenv_path, '/home/deploy/.rbenv/'
+
 namespace :deploy do
 
   desc 'Restart application'
